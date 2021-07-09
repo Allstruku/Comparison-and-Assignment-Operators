904e58412bfcd2e64ba279a7953ae95674a7ccd3
ParkJooahn <ParkJooahn@localhost>
2021-07-09 14:23:56 +0900

    Initial Commit

---
+++ src/ComparisonOperatorsExam.java
@@ -0,0 +1,21 @@
+
+public class ComparisonOperatorsExam {
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 10;
+		System.out.println(i == j);	//two equal signs indicate that the two values are equal
+		System.out.println(i != j);	//an exclamation point before the equal sign indicates that the two values are not equal
+		System.out.println(i < j);	//this sign indicates that the value on the right is larger than that on the left
+		System.out.println(i <= j);	//this sign indicates that the value on the right is larger or equal than that on the left
+		System.out.println(i > j);	//this sign indicates that the value on the right is smaller than that on the left
+		System.out.println(i >= j);	//this sign indicates that the value on the right is smaller or equal than that on the left
+		
+		
+		i += 10; //복합대입연산자 Compound Assignment Operator += indicates that the value of i + 10 will be assigned the variable of i
+		System.out.println(i);
+		
+	
+	
+	
+	}
+}
